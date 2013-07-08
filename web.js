d54e455b351e9b8cfd01a3d9dbc26fe2970c565a
MSYMR <ymreaux1962@yahoo.com>
2013-07-08 04:41:13 +0000

    Modified to read from index.html.

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(256);
+
+buf = fs.readFileSync('./index.html');
+
+var str = buf.toString('utf8', 0, buf.length);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
